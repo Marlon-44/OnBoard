ef33cf9dd39fd7b9908dffc74366d13aac7fd526
Felipe Eraso <felipeeraso.navarro@gmail.com>
2025-06-16 23:33:10 -0500

    .
    .

--- backend/src/main/java/com/onboard/backend/entity/EstadoAlquiler.java
+++ backend/src/main/java/com/onboard/backend/entity/EstadoAlquiler.java
@@ -7,6 +7,5 @@ public enum EstadoAlquiler {
     RETRASADO,            // Cliente no ha devuelto el vehículo a tiempo
     NO_DEVUELTO,          // Cliente nunca devolvió el vehículo (se asume pérdida o robo)
     VEHICULO_DANADO,      // Vehículo devuelto con daños significativos
-    ABANDONADO,           // Cliente abandonó el contrato o dejó el vehículo sin informar
     INCIDENTE_GRAVE       // Se reportó un accidente o situación legal importante durante el alquiler
 }
