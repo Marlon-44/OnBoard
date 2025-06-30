0055fa718452dbf718320b311005c54a4d844d99
Marlon-44 <marlondavidag44@outlook.com>
2025-06-30 16:53:41 -0500

    ...

--- src/api/factura.js
+++ src/api/factura.js
@@ -2,6 +2,13 @@ import axios from "axios";
 const API = "http://localhost:8080/api";
 
 export const obtenerFacturaPorReserva = async (idReserva) => {
-    const response = await axios.get(`${API}/reservas/${idReserva}/factura`);
-    return response.data; // debe contener la factura con idFactura
+    try {
+        const response = await axios.get(`http://localhost:8080/api/reservas/${idReserva}/factura`);
+        console.log("Exito, factura: ", response.data)
+        return response.data; // Se espera que contenga la factura con idFactura
+    } catch (error) {
+        console.error(`Error al obtener la factura para la reserva ${idReserva}:`, error);
+        throw new Error("No se pudo obtener la factura desde el servidor.");
+    }
 };
+
